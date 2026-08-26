72999c2b341ca0684b90beaa68350436290a807f
Joownlok <joao.6564693@gmail.com>
2026-08-26 09:37:31 -0300

    Exercício Python Curso em Vídeo 003

---
+++ python/Python Curso em Video/ex005.py
@@ -0,0 +1,5 @@
+# Somando dois números
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite um segundo número: '))
+total = n1 + n2
+print(f'A soma entre os 2 números é igual a {total:.2}')
