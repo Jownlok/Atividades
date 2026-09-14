e4f7f29fe98584f3f892fe5d971db413e23c587d
Joownlok <joao.6564693@gmail.com>
2026-09-14 18:18:48 -0300

    Aula 02 JavaScript - Tipos de saída = innerHTML, Alert, Console, document.write()

---
+++ JavaScript/js Mundial Cursos/aula02.js
@@ -0,0 +1,11 @@
+// Tipos de Saída (Em JS são diferentes minusculo/maiusculo, ex.: Oi, oi são diferentes)
+//Selecionando parágrafo
+document.querySelector("#Texto").innerHTML= "Meu primeiro texto <b>JS!</b>";
+//innerHTML = inserindo / escrevendo / sobscrevendo
+
+// Outro tipo de saída = alertas
+alert(10+5);
+alert("Olá, mundo!");
+
+// Tipo de saída console
+console.log("Olá, pelo console.");
